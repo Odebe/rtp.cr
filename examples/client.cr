c06ded74c747b0216740fbe7610955f61f795391
Mihail Odebe <derpiranha@gmail.com>
2021-07-27 23:58:33 +0300

    example fix, lol

--- examples/client.cr
+++ examples/client.cr
@@ -1,23 +1,21 @@
 require "../src/rtp.cr"
 
-puts "cluent"
+client = Rtp::Client.new("localhost", 1337)
+client.connect!
 
-# client = Rtp::Client.new("localhost", 1337)
-# client.connect!
+packet = Rtp::Packet.new
+packet.version = 2
+packet.padding = false
+packet.extension = false
+packet.csrc_count = 0
+packet.marker = false
+packet.payload_type = 9
+packet.seq_num = 333
+packet.timestamp = 123
+packet.ssrc_ident = 1337
+packet.payload = Bytes[1,2,3,4,5,6]
 
-# packet = Rtp::Packet.new
-# packet.version = 2
-# packet.padding = false
-# packet.extension = false
-# packet.csrc_count = 0
-# packet.marker = false
-# packet.payload_type = 9
-# packet.seq_num = 333
-# packet.timestamp = 123
-# packet.ssrc_ident = 1337
-# packet.payload = Bytes[1,2,3,4,5,6]
+client.send(packet)
 
-# client.send(packet)
-
-# puts client.receive.inspect
-# client.close
+puts client.receive.inspect
+client.close
