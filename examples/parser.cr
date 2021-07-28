2adb738487d1ad60e79449d971808dcb1a66e331
Mihail Odebe <derpiranha@gmail.com>
2021-07-28 13:33:26 +0300

    parser example

---
+++ examples/parser.cr
@@ -0,0 +1,21 @@
+require "../src/rtp.cr"
+
+packet = Rtp::Packet.new
+packet.version = 2
+packet.padding = false
+packet.extension = false
+packet.csrc_count = 0
+packet.marker = false
+packet.payload_type = 9
+packet.seq_num = 333
+packet.timestamp = 123
+packet.ssrc_ident = 1337
+packet.payload = Bytes[1,2,3,4,5,6]
+
+puts packet.inspect
+
+bytes = Rtp::Parser.encode(packet)
+puts bytes.inspect
+
+packet2 = Rtp::Parser.decode(bytes)
+puts packet2.inspect
